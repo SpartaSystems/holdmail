785a3d5d009eb7bcbc57c84d4bce7e21e5859e1d
Kurt Bliefernich <kurt.bliefernich@spartasystems.com>
2016-06-13 22:43:59 -0400

    Added unit tests on message mapper

---
+++ src/test/java/com/spartasystems/holdmail/persistence/MessageEntityTest.java
@@ -0,0 +1,16 @@
+package com.spartasystems.holdmail.persistence;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.Test;
+
+public class MessageEntityTest {
+
+    @Test
+    public void shouldVerifyEqualsContract() {
+        EqualsVerifier.forClass(MessageEntity.class)
+                .suppress(Warning.NONFINAL_FIELDS, Warning.NULL_FIELDS)
+                .verify();
+    }
+
+}
