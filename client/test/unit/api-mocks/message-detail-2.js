194373225313ac9b80609979bb8c2bfa97547794
Tim Sneed <timothy.sneed@spartasystems.com>
2017-10-30 11:32:49 -0400

    general cleanup

--- client/test/unit/api-mocks/message-detail-2.js
+++ client/test/unit/api-mocks/message-detail-2.js
@@ -18,19 +18,19 @@
 /*eslint-disable */
 export default {
    "messageId":63,
-   "identifier":"<660c8d85-7c5c-1499-3fbf-78b10cdfcc56@mailhold.com>",
+   "identifier":"<660c8d85-7c5c-1499-3fbf-78b10cdfcc56@holdmail.com>",
    "subject":"HEYOOOO",
    "senderEmail":"test@test.com",
    "receivedDate":1490966771070,
    "senderHost":"0:0:0:0:0:0:0:1",
    "messageSize":1279,
    "recipients":"test@test.com",
-   "messageRaw":"Received: from local (localhost [0:0:0:0:0:0:0:1])\r\n        by 10.0.5.49\r\n        with SMTP (HoldMailSMTPServer SMTP) id J0XV4B7O\r\n        for test@test.com;\r\n        Fri, 31 Mar 2017 09:26:11 -0400 (EDT)\r\nTo: test@test.com\r\nFrom: Tim S <test@test.com>\r\nSubject: HEYOOOO\r\nMessage-ID: <660c8d85-7c5c-1499-3fbf-78b10cdfcc56@mailhold.com>\r\nDate: Fri, 31 Mar 2017 09:26:10 -0400\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:45.0)\r\n Gecko/20100101 Thunderbird/45.8.0\r\nMIME-Version: 1.0\r\nContent-Type: multipart/alternative;\r\n boundary=\"------------0D39BD9051D59F0C8B9335BB\"\r\n\r\nThis is a multi-part message in MIME format.\r\n--------------0D39BD9051D59F0C8B9335BB\r\nContent-Type: text/plain; charset=utf-8; format=flowed\r\nContent-Transfer-Encoding: 7bit\r\n\r\nHi\r\n\r\n\r\nAll these colors\r\n\r\n\r\n--------------0D39BD9051D59F0C8B9335BB\r\nContent-Type: text/html; charset=utf-8\r\nContent-Transfer-Encoding: 7bit\r\n\r\n<html>\r\n  <head>\r\n\r\n    <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">\r\n  </head>\r\n  <body bgcolor=\"#FFFFFF\" text=\"#000000\">\r\n    <p><font color=\"#ffcc00\">Hi</font></p>\r\n    <p><br>\r\n    </p>\r\n    <p><font color=\"#cc0000\">All these colors</font><br>\r\n    </p>\r\n  </body>\r\n</html>\r\n\r\n--------------0D39BD9051D59F0C8B9335BB--\r\n",
+   "messageRaw":"Received: from local (localhost [0:0:0:0:0:0:0:1])\r\n        by 10.0.5.49\r\n        with SMTP (HoldMailSMTPServer SMTP) id J0XV4B7O\r\n        for test@test.com;\r\n        Fri, 31 Mar 2017 09:26:11 -0400 (EDT)\r\nTo: test@test.com\r\nFrom: Tim S <test@test.com>\r\nSubject: HEYOOOO\r\nMessage-ID: <660c8d85-7c5c-1499-3fbf-78b10cdfcc56@holdmail.com>\r\nDate: Fri, 31 Mar 2017 09:26:10 -0400\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:45.0)\r\n Gecko/20100101 Thunderbird/45.8.0\r\nMIME-Version: 1.0\r\nContent-Type: multipart/alternative;\r\n boundary=\"------------0D39BD9051D59F0C8B9335BB\"\r\n\r\nThis is a multi-part message in MIME format.\r\n--------------0D39BD9051D59F0C8B9335BB\r\nContent-Type: text/plain; charset=utf-8; format=flowed\r\nContent-Transfer-Encoding: 7bit\r\n\r\nHi\r\n\r\n\r\nAll these colors\r\n\r\n\r\n--------------0D39BD9051D59F0C8B9335BB\r\nContent-Type: text/html; charset=utf-8\r\nContent-Transfer-Encoding: 7bit\r\n\r\n<html>\r\n  <head>\r\n\r\n    <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">\r\n  </head>\r\n  <body bgcolor=\"#FFFFFF\" text=\"#000000\">\r\n    <p><font color=\"#ffcc00\">Hi</font></p>\r\n    <p><br>\r\n    </p>\r\n    <p><font color=\"#cc0000\">All these colors</font><br>\r\n    </p>\r\n  </body>\r\n</html>\r\n\r\n--------------0D39BD9051D59F0C8B9335BB--\r\n",
    "messageHeaders":{
       "Received":"from local (localhost [0:0:0:0:0:0:0:1])\r\n        by 10.0.5.49\r\n        with SMTP (HoldMailSMTPServer SMTP) id J0XV4B7O\r\n        for test@test.com;\r\n        Fri, 31 Mar 2017 09:26:11 -0400 (EDT)",
       "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:45.0)\r\n Gecko/20100101 Thunderbird/45.8.0",
       "To":"test@test.com",
-      "Message-ID":"<660c8d85-7c5c-1499-3fbf-78b10cdfcc56@mailhold.com>",
+      "Message-ID":"<660c8d85-7c5c-1499-3fbf-78b10cdfcc56@holdmail.com>",
       "From":"Tim <test@test.com>",
       "Subject":"HEYOOOO",
       "MIME-Version":"1.0",
